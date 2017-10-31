8ba50754769c1b61c6fa275aad113b1a25c0d2e6
Camilo vargas <ecvargase@unal.edu.co>
2017-10-31 16:09:46 -0500

    add test espresso

---
+++ app/src/androidTest/java/com/espiot/cav/carwifi/MainActivityTest.java
@@ -0,0 +1,49 @@
+package com.espiot.cav.carwifi;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+
+        ViewInteraction dir = onView(
+                allOf(withId(R.id.joys), isDisplayed()));
+        dir.check(matches(isDisplayed()));
+
+        ViewInteraction vel = onView(
+                allOf(withId(R.id.seek_arc), isDisplayed()));
+        vel.check(matches(isDisplayed()));
+
+        ViewInteraction toggleButton = onView(
+                allOf(withId(R.id.toggleButton), withText("No"),
+                        isDisplayed()));
+        toggleButton.check(matches(isDisplayed())).perform(click());
+
+        ViewInteraction toggleButton2 = onView(
+                allOf(withId(R.id.toggleButton), withText("Sí"), isDisplayed()));
+        toggleButton2.check(matches(isDisplayed())).perform(click());
+
+    }
+}
