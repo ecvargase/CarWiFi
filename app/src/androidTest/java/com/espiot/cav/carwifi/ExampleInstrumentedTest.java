a9402cd9bd91ae41f9d0b0bceb2de14089e3a038
Camilo vargas <ecvargase@unal.edu.co>
2017-10-31 11:38:19 -0500

    add travis and sync libs

--- app/src/androidTest/java/com/espiot/cav/carwifi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/espiot/cav/carwifi/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.janis.sac.smartswitch", appContext.getPackageName());
+        assertEquals("carwifi.cav.carwifi", appContext.getPackageName());
     }
 }
