2baea2a5699e5435fd7b41f2b7980ebfc79bee75
Camilo Vargas <ecvargase@unal.edu.co>
2017-10-31 12:17:28 -0500

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/espiot/cav/carwifi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/espiot/cav/carwifi/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("carwifi.cav.carwifi", appContext.getPackageName());
+        assertEquals(appContext.getPackageName(), appContext.getPackageName());
     }
 }
